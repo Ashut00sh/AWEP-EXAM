839bec9c1c0c599c2c546496e1c2f079671b1ba6
Ashutosh Jadhav <65148544+Ashut00sh@users.noreply.github.com>
2020-08-16 12:17:17 +0530

    Add files via upload

---
+++ 200240320024/2.js
@@ -0,0 +1,9 @@
+let mail = function (abc) {
+  console.log(abc.parentElement.parentElement.parentElement.children[1]);
+};
+let pass = function (pqr) {
+  // console.log(pqr);
+};
+let btn = function (asd) {
+  // console.log(asd);
+};
